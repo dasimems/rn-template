0d63df200bc0719587bb69c71d080b44b595694d
Duyil Ayomid <isaacseun63@gmail.com>
2024-06-12 11:03:09 +0100

    update: added some new functions to help make development easier

--- utils/functions.ts
+++ utils/functions.ts
@@ -1,6 +1,9 @@
-import { LayoutChangeEvent, Vibration } from "react-native";
+import { LayoutChangeEvent, Share, Vibration } from "react-native";
 import { VibrationTypes } from "./types";
 import { vibrationLengths } from "./_variables";
+import Toast, { ToastOptions } from "react-native-root-toast";
+import * as WebBrowser from "expo-web-browser";
+import { emailRegExp, numberRegExp } from "./regex";
 
 export const backspaceText: (
   text: string,
@@ -78,4 +81,264 @@ export const formatSeconds = (sec: number) => {
     }
   }
   return `${formatedSec}${secType}`;
+};
+
+export const formatText = (text: string) => {
+  const formatedText = text
+    ? `${text.slice(0, 1).toUpperCase()}${text.slice(1)}`
+    : "";
+
+  return formatedText;
+};
+
+export const stripText = (text: string, max: number = 15) => {
+  const stripedText = text
+    ? `${text.slice(0, max)}${text.length > max ? "..." : ""}`
+    : "";
+
+  return stripedText;
+};
+
+export const formatPhoneNumber = (phoneNumber: string): string => {
+  let newPhoneNumber = "";
+
+  phoneNumber.split("").forEach((char) => {
+    const isNumber = numberRegExp.test(char);
+
+    if (isNumber) {
+      newPhoneNumber += char;
+    }
+  });
+
+  return newPhoneNumber;
+};
+
+export const showToast = (message: string, options?: ToastOptions): void => {
+  Toast.show(message, {
+    position: Toast.positions.BOTTOM,
+    animation: true,
+    hideOnPress: true,
+    ...options
+  });
+};
+
+export const validateEmail = (email: string) => {
+  return String(email).toLowerCase().match(emailRegExp);
+};
+
+export const generateFileBlob = (
+  uri: string,
+  expectedMiMeType:
+    | "text"
+    | "application"
+    | "image"
+    | "audio"
+    | "video" = "image"
+) => {
+  const name = uri.split("/").pop();
+  const match = /\.(\w+)$/.exec(name as string);
+  const type = match ? `${expectedMiMeType}/${match[1]}` : `image`;
+  // console.log(uri);
+  return {
+    uri,
+    name,
+    type
+  } as unknown as Blob;
+};
+
+export const validateValues: <T>(
+  data: T | any,
+  validation?: {
+    [name: string]:
+      | {
+          required?: boolean & {
+            value: boolean;
+            message: string;
+          } & any;
+          regex?: RegExp & {
+            value: RegExp;
+            message: string;
+          } & any;
+          minLength?: number & {
+            value: number;
+            message: string;
+          } & any;
+          maxLength?: number & {
+            value: number;
+            message: string;
+          } & any;
+          min?: number & {
+            value: number;
+            message: string;
+          } & any;
+          max?: number & {
+            value: number;
+            message: string;
+          } & any;
+        } & boolean &
+          any;
+  }
+) => {
+  [name: string]: string;
+} | null = (data, validation) => {
+  let error: { [name: string]: string } | null = null;
+
+  if (!data || typeof data !== "object") {
+    data = {};
+  }
+
+  if (
+    data &&
+    typeof data === "object" &&
+    validation &&
+    typeof validation === "object"
+  ) {
+    const validationKeys = Object.keys(validation);
+
+    validationKeys.forEach((key: string) => {
+      const value = data[key];
+      const validationValue = validation[key];
+
+      if (validationValue) {
+        if (
+          typeof validationValue !== "object" &&
+          (!value || value.length < 1)
+        ) {
+          error = {
+            ...error,
+            [key]: `Please provide your ${key}`
+          };
+        } else {
+          if (
+            (validationValue.required || validationValue?.required?.value) &&
+            (!value || value.length < 1)
+          ) {
+            error = {
+              ...error,
+              [key]:
+                validationValue.required.message || `Please provide your ${key}`
+            };
+          } else if (
+            !isNaN(Number(value)) &&
+            ((validationValue.min &&
+              !isNaN(Number(validationValue.min)) &&
+              Number(value) < validationValue.min) ||
+              (validationValue?.min?.value &&
+                !isNaN(Number(validationValue?.min?.value)) &&
+                Number(value) < validationValue.min.value))
+          ) {
+            error = {
+              ...error,
+              [key]:
+                validationValue?.min?.message ||
+                `Your ${key} must not be less than ${validation.min}`
+            };
+          } else if (
+            !isNaN(Number(value)) &&
+            ((validationValue.max &&
+              !isNaN(Number(validationValue.max)) &&
+              Number(value) > validationValue.max) ||
+              (validationValue?.max?.value &&
+                !isNaN(Number(validationValue?.max?.value)) &&
+                Number(value) > validationValue?.max?.value))
+          ) {
+            error = {
+              ...error,
+              [key]:
+                validationValue?.max?.message ||
+                `Your ${key} must not be greater than ${validation.max}`
+            };
+          } else if (
+            (validationValue.minLength &&
+              !isNaN(Number(validationValue.minLength)) &&
+              value.toString().length < validationValue.minLength) ||
+            (validationValue?.minLength?.value &&
+              !isNaN(Number(validationValue?.minLength?.value)) &&
+              value.toString().length < validationValue?.minLength?.value)
+          ) {
+            error = {
+              ...error,
+              [key]:
+                validationValue?.minLength?.message ||
+                `Your ${key} must not be less than ${validation.minLength} characters`
+            };
+          } else if (
+            (validationValue.maxLength &&
+              !isNaN(Number(validationValue.maxLength)) &&
+              value.toString().length > validationValue.maxLength) ||
+            (validationValue?.maxLength?.value &&
+              !isNaN(Number(validationValue?.maxLength?.value)) &&
+              value.toString().length > validationValue?.maxLength?.value)
+          ) {
+            error = {
+              ...error,
+              [key]:
+                validationValue?.maxLength?.message ||
+                `Your ${key} must not be greater than ${validation.maxLength} characters`
+            };
+          } else if (
+            (validationValue.regex &&
+              !validationValue?.regex?.value &&
+              !validationValue.regex.test(value)) ||
+            (validationValue?.regex?.value &&
+              !validationValue?.regex?.value.test(value))
+          ) {
+            error = {
+              ...error,
+              [key]:
+                validationValue?.regex?.message || `Please input a valid ${key}`
+            };
+          }
+        }
+      }
+    });
+  }
+
+  return error;
+};
+
+export const shareContent = async (
+  message: string,
+  url?: string,
+  title?: string
+) => {
+  if (message) {
+    try {
+      const result = await Share.share({
+        message,
+        url,
+        title
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+        } else {
+          // showToast("Error encountered while sharing");
+        }
+      } else if (result.action === Share.dismissedAction) {
+        showToast("Operation canceled by user");
+      }
+    } catch (error) {
+      showToast("Error encountered while sharing");
+    }
+  } else {
+    showToast("No Message passed");
+  }
+};
+export const openLinkInBrowser = (link: string) => {
+  WebBrowser.openBrowserAsync(link).catch((err) => {
+    showToast("Error opening link");
+  });
+};
+
+export const stripPhoneNumber = (number: string) => {
+  let phoneNumber = "";
+
+  if (
+    number.toString().slice(0, 1) === "0" ||
+    number.toString().slice(0, 1) === "+"
+  ) {
+    phoneNumber = number.slice(1);
+  }
+
+  return phoneNumber;
 };
